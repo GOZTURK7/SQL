d24981abbd307a3ea21b1aaaacf516616388bbdd
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2022-01-05 00:39:48 +0300

    4'th January

---
+++ Day5_ALTER_TABLE.sql
@@ -0,0 +1,33 @@
+USE sys;
+/*=============================== ALTER TABLE ==================================
+    
+    ALTER TABLE  tabloda ADD, MODIFY, veya DROP/DELETE COLUMNS islemleri icin 
+    kullanilir.
+    
+    ALTER TABLE ifadesi tablolari yeniden isimlendirmek (RENAME) icin de
+    kullanilir.
+   
+==============================================================================*/
+
+    CREATE TABLE personel
+    (
+        id int PRIMARY KEY , 
+        isim VARCHAR(50), 
+        sehir VARCHAR(50), 
+        maas int, 
+        sirket VARCHAR(20)
+    );
+    
+    INSERT INTO personel VALUES(123456789, 'Ali Yilmaz', 'Istanbul', 5500, 'Honda');
+    INSERT INTO personel VALUES(234567890, 'Veli Sahin', 'Istanbul', 4500, 'Toyota');
+    INSERT INTO personel VALUES(345678901, 'Mehmet Ozturk', 'Ankara', 3500, 'Honda'); 
+    INSERT INTO personel VALUES(456789012, 'Mehmet Ozturk', 'Izmir', 6000, 'Ford');
+    INSERT INTO personel VALUES(567890123, 'Mehmet Ozturk', 'Ankara', 7000, 'Tofas');
+    INSERT INTO personel VALUES(456715012, 'Veli Sahin', 'Ankara', 4500, 'Ford');
+    
+    select * from personel;
+    
+    
+    
+    
+    
